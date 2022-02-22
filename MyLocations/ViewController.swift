fcd486f83ced62f9a874c4e7c78a3f055e67fbc7
Micah Pennington <mpennin3@hotmail.com>
2022-02-22 18:36:17 -0500

    Initial Commit

---
+++ MyLocations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyLocations
+//
+//  Created by Micah on 2/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
